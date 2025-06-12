8dc9c3cd670dddc31334089fdb831de37ac53684
Hossein Ghoreyshi <hosssein.ghoreyshi456@gmail.com>
2025-06-12 16:23:03 +0330

    refactor: update DashboardPage to render DashboardClient component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import DashboardClient from "@/components/dashboard/dashboard-client";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -6,5 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default function DashboardPage() {
-  return <></>
+  return <DashboardClient />;
 }
