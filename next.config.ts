9ab9f56d95fd700270cec93a9834f9bece90ea81
Hossein Ghoreyshi <hosssein.ghoreyshi456@gmail.com>
2025-06-12 17:35:38 +0330

    refactor: remove Image component and revert to img tag for user avatar in DashboardClient

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-        port: "",
-        pathname: "/api/portraits/**",
-        search: "",
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
