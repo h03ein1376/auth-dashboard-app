420a85a1c4eceb0984ce7c8e4a0566c09d70746d
Hossein Ghoreyshi <hosssein.ghoreyshi456@gmail.com>
2025-06-12 16:21:12 +0330

    feat: add styling and layout for AuthPage component

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,3 +1,14 @@
+import AuthForm from "@/components/auth-form/auth-form";
+import styles from "./auth.module.scss";
+
 export default function AuthPage() {
-  return <></>;
+  return (
+    <div className={styles.authContainer}>
+      <div className={styles.authBox}>
+        <h1>ورود به سیستم</h1>
+        <p>برای ورود، شماره تلفن خود را وارد کرده و روی دکمه ورود کلیک کنید.</p>
+        <AuthForm />
+      </div>
+    </div>
+  );
 }
