8270e6643fadcfc3d7d81d0a8c1161dc971d8a94
Hossein Ghoreyshi <hosssein.ghoreyshi456@gmail.com>
2025-06-12 16:15:15 +0330

    refactor: update metadata for layout and dashboard pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "داشبورد کاربری",
+  description: "داشبورد کاربری شما برای مدیریت حساب.",
+};
 
 export default function DashboardPage() {
   return <></>
