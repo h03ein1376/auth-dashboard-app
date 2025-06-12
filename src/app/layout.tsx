daad8e0551f57242b7921529087fa395d9f16822
Hossein Ghoreyshi <hosssein.ghoreyshi456@gmail.com>
2025-06-12 16:13:37 +0330

    refactor: add scss, remove unused styles and code and config lalehzar as forn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Lalezar } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const lalezar = Lalezar({
+  subsets: ["arabic"],
+  weight: "400",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="fa" dir="rtl">
+      <body className={lalezar.className}>
         {children}
       </body>
     </html>
